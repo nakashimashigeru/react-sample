461c4b83717fbf104841b02d0036bf6b5682b1da
nakashimashigeru <nakashimashigeru1951.9.17@gmail.com>
2022-02-06 22:00:05 +0900

    子エレメントを活用する

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import List from './List';
+import Message from './Message';
 
 class App extends Component {
-  data = [
-    "This is list sample.",
-    "これはリストのサンプルです。",
-    "配列をリストに変換します。"
-  ];
+  input = '';
 
   constructor(props) {
     super(props);
-    this.state = {
-      list: this.data
-    };
   }
 
   render() {
     return <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className='subtitle'>Show List.</p>
-        <List title="サンプル・リスト" data={this.data} />
+        <Message title="Children!">
+          これはコンポーネント内のコンテンツです。
+          マルでテキストを分割し、リストにして表示します。
+          改行は必要ありません。
+        </Message>
       </div>
     </div>
   }
